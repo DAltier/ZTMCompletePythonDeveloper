9bedaa2554075ff296f13ef1099de4c7320c647e
DAltier <altierdana@gmail.com>
2025-06-03 22:18:14 -0700

    password checker

---
+++ 001_basics_1/password_checker.py
@@ -0,0 +1,4 @@
+username = input("What is your username? ")
+password = input("What is your password? ")
+
+print(f"{username}, your password, {'*'*len(password)}, is {len(password)} characters long.")
