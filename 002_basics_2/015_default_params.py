fd928c71debddc1a8ec977963cba655b1e200e34
DAltier <altierdana@gmail.com>
2025-07-03 20:57:43 -0700

    default params

---
+++ 002_basics_2/015_default_params.py
@@ -0,0 +1,12 @@
+#parameters
+def say_hello(name='Darth Vader', emoji='^'):
+	print(f'Hello {name} {emoji}')
+
+#arguments
+say_hello('Dana', '*')
+
+#keyword arguments
+say_hello(name='Bibi', emoji='@')
+
+
+say_hello()
