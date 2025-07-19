bd646fa8dda1a6f313c68acfad3b656ae64325f1
DAltier <altierdana@gmail.com>
2025-07-19 13:09:13 -0700

    sets methods

--- 001_basics_1/030_sets_methods.py
+++ 001_basics_1/030_sets_methods.py
@@ -4,47 +4,38 @@ my_set = {1,2,3,4,5}
 your_set = {4,5,6,7,8,9,10}
 
 # .difference()
-# .discard()
-# .difference_update()
-# .intersection()
-# .isdisjoint()
-# .issubset()
-# .issuperset()
-# .union()
-
-print("\ndifference")
 print(my_set.difference(your_set))
 print(my_set)
 
-print("\ndiscard")
+# .discard()
 print(my_set.discard(5))
 print(my_set)
 
-print("\ndifference_update")
+# .difference_update()
 print(my_set.difference_update(your_set))
 print(my_set)
 
 my_set = {1,2,3,4,5}
 
-print("\nintersection")
+# .intersection()
 print(my_set.intersection(your_set))
 print(my_set & your_set)
 
-print("\nisdisjoint")
+# .isdisjoint()
 print(my_set.isdisjoint(your_set))
 my_set = {1,2,3}
 print(my_set.isdisjoint(your_set))
 
-print("\nissubset")
+# .issubset()
 print(my_set.issubset(your_set))
 my_set = {4,6,8}
 print(my_set.issubset(your_set))
 
-print("\nissuperset")
+# .issuperset()
 print(my_set.issuperset(your_set))
 print(your_set.issuperset(my_set))
 
-print("\nunion")
+# .union()
 my_set = {1,2,3,4,5}
 print(my_set.union(your_set))
 # or
