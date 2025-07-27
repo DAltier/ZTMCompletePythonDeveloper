567b86be3d1a520a2da0d62690a6955e304653be
DAltier <altierdana@gmail.com>
2025-07-26 22:30:03 -0700

    mro

--- 003_oop/017_method_resolution_order.py
+++ 003_oop/017_method_resolution_order.py
@@ -15,3 +15,14 @@ print(D.num)
 print(D.mro())
 print(D.__mro__)
 # both the above are same
+
+class X:pass
+class Y:pass
+class Z:pass
+class A(X,Y):pass
+class B(Y,Z):pass
+class M(B,A,Z):pass
+
+print(M.mro())
+
+# M -> B -> A -> X -> Y -> Z -> object
