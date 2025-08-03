b0f4ee5a02c63ae03b34f9a8be03377abc8af018
DAltier <altierdana@gmail.com>
2025-08-02 21:55:18 -0700

    list comprehensions

---
+++ 004_functional_programming/009_list_comprehensions.py
@@ -0,0 +1,12 @@
+my_list = []
+
+for item in 'hello':
+    my_list.append(item)
+
+print(my_list)
+
+my_list1 = [item for item in 'Andrei']
+print(my_list1)
+
+my_list2 = [num**2 for num in range(1,11)]
+print(my_list2)
