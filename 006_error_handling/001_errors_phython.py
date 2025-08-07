8948e93dd92a5f8071190c8396488b5f1e835de2
DAltier <altierdana@gmail.com>
2025-08-06 22:18:02 -0700

    errors in python

---
+++ 006_error_handling/001_errors_phython.py
@@ -0,0 +1,14 @@
+# Error Handling
+
+print(1 + 'hello') # type error
+
+#def hoohoo() # syntax error
+	#pass
+
+li = [1, 2, 3]
+li[5] # index out of range error
+
+di = {'a': 1}
+di['b'] # key error
+
+5 / 0 # division by zero error
