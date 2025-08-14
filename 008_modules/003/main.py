d0b5b8641c437dc5c6c5591e5d09e6c841705b2b
DAltier <altierdana@gmail.com>
2025-08-13 22:24:20 -0700

    ways to import

---
+++ 008_modules/003/main.py
@@ -0,0 +1,10 @@
+# from utility import multiply, divide
+# from shopping.more_shopping.shopping_cart import buy
+
+# print(multiply(2,3))
+# print(divide(5,2))
+# print(buy('apple'))
+
+from shopping.more_shopping import shopping_cart
+
+print(shopping_cart.buy('apple'))
