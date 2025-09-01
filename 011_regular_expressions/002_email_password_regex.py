54cbd2f6a5d0a44a4a8496faa03f8a183c99696f
DAltier <altierdana@gmail.com>
2025-08-31 20:51:23 -0700

    unit testing

--- 011_regular_expressions/002_email_password_regex.py
+++ 011_regular_expressions/002_email_password_regex.py
@@ -3,8 +3,11 @@ import re
 email_pattern = re.compile(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
 check_email = email_pattern.fullmatch('saurabh_1089@gmail.com')
 
-password_patter = re.compile(r"([a-zA-Z0-9@#$%]{8,}$)")
-check_password = password_patter.fullmatch('12345678')
+# at least 8 characters long
+# contains any sort of letter, numbers, $%#@
+# has to end with a number
+password_patter = re.compile(r"([a-zA-Z0-9@#$%]{8,}\d)")
+check_password = password_patter.fullmatch('edtrswe#$%9')
 
 if check_email and check_password:
     print("Both email and password are correct.")
