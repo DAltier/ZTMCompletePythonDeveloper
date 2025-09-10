641f77537408a4eb091b63b11b807dcd29359895
DAltier <altierdana@gmail.com>
2025-09-09 20:46:04 -0700

    jpg to png converter

--- 013_scripting/001_pokedex/JPGtoPNGconverter.py
+++ 013_scripting/001_pokedex/JPGtoPNGconverter.py
@@ -2,17 +2,70 @@ import sys
 import os
 from PIL import Image
 
-path = sys.argv[1]
-directory = sys.argv[2]
+# Optional: GUI folder pickers if no args given
+def pick_dirs_with_gui():
+    try:
+        import tkinter as tk
+        from tkinter import filedialog
+        root = tk.Tk()
+        root.withdraw()
+        in_dir = filedialog.askdirectory(title="Select input folder (JPG/JPEG)")
+        if not in_dir:
+            return None, None
+        out_dir = filedialog.askdirectory(title="Select output folder (PNG)")
+        if not out_dir:
+            return None, None
+        return in_dir, out_dir
+    except Exception:
+        return None, None
 
-if not os.path.exists(directory):
-    os.makedirs(directory)
-    
+# 1) Get input/output directories from args or GUI prompts
+if len(sys.argv) >= 3:
+    input_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+else:
+    # Try GUI pickers; if canceled, show usage and exit
+    input_dir, output_dir = pick_dirs_with_gui()
+    if not input_dir or not output_dir:
+        print("Usage: python JPGtoPNGconverter.py <input_folder> <output_folder>")
+        sys.exit(1)
 
-for filename in os.listdir(path):
-  clean_name = os.path.splitext(filename)[0]
-  img = Image.open(f'{path}{filename}')
-  #added the / in case user doesn't enter it. You may want to check for this and add or remover it. 
-  img.save(f'{directory}/{clean_name}.png', 'png')
-  print('all done!')
+# 2) Validate and prepare output folder
+if not os.path.isdir(input_dir):
+    print(f"Input folder not found: {input_dir}")
+    sys.exit(1)
 
+os.makedirs(output_dir, exist_ok=True)
+
+# 3) Convert images
+valid_exts = {".jpg", ".jpeg", ".JPG", ".JPEG"}
+converted = 0
+skipped = 0
+
+for filename in os.listdir(input_dir):
+    file_path = os.path.join(input_dir, filename)
+    if not os.path.isfile(file_path):
+        continue
+
+    name, ext = os.path.splitext(filename)
+    if ext not in valid_exts:
+        skipped += 1
+        continue
+
+    try:
+        with Image.open(file_path) as img:
+            # Ensure RGB so JPEGs with no alpha save cleanly as PNG
+            if img.mode in ("RGBA", "RGB", "L"):
+                pass
+            else:
+                img = img.convert("RGB")
+
+            save_path = os.path.join(output_dir, f"{name}.png")
+            img.save(save_path, "PNG")
+            print(f"Converted: {filename} -> {save_path}")
+            converted += 1
+    except Exception as e:
+        print(f"Skipping {filename}: {e}")
+        skipped += 1
+
+print(f"All done! Converted {converted} file(s). Skipped {skipped}.")
--- 013_scripting/001_pokedex/images.py
+++ 013_scripting/001_pokedex/images.py
@@ -1,34 +1,35 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('./Pokedex/pikachu.jpg')
+img = Image.open(r"c:\Users\cnx36\Documents\7.Projects\1.ZeroToMastery\CompletePythonDeveloper\013_scripting\001_pokedex\Pokedex\pikachu.jpg")
+
 print(img)
 print(img.format)
 print(img.size)
 print(img.mode)
 
 filtered_img = img.filter(ImageFilter.BLUR)
-filtered_img.save("./Pokedex/blur.png", "png")
+filtered_img.save(r"c:\Users\cnx36\Documents\7.Projects\1.ZeroToMastery\CompletePythonDeveloper\013_scripting\001_pokedex\Pokedex\blur.png", "png")
 
 filtered_img = img.filter(ImageFilter.SMOOTH)
-filtered_img.save("./Pokedex/smooth.png", "png")
+filtered_img.save(r"c:\Users\cnx36\Documents\7.Projects\1.ZeroToMastery\CompletePythonDeveloper\013_scripting\001_pokedex\Pokedex\smooth.png", "png")
 
 filtered_img = img.filter(ImageFilter.SHARPEN)
-filtered_img.save("./Pokedex/sharpen.png", "png")
+filtered_img.save(r"c:\Users\cnx36\Documents\7.Projects\1.ZeroToMastery\CompletePythonDeveloper\013_scripting\001_pokedex\Pokedex\sharpen.png", "png")
 
-filtered_img = img.convert('L')  # it converts the image to grey scale, that is Black and White. Similarly RGB = Red Green Blue
-filtered_img.save("./Pokedex/grey.png", "png")
+filtered_img = img.convert('L') # it converts the image to grey scale, that is Black and White. Similarly RGB = Red Green Blue
+filtered_img.save(r"c:\Users\cnx36\Documents\7.Projects\1.ZeroToMastery\CompletePythonDeveloper\013_scripting\001_pokedex\Pokedex\grey.png", "png")
 
-# filtered_img.show()    # this opens the image in the default player
+img.show() # this opens the image in the default player
 
-rotated_img = img.rotate(45)
-rotated_img.save("./Pokedex/rotated.png", "png")
+crooked_img = img.rotate(180)
+crooked_img.save(r"c:\Users\cnx36\Documents\7.Projects\1.ZeroToMastery\CompletePythonDeveloper\013_scripting\001_pokedex\Pokedex\rotated.png", "png")
 
-resized_img = img.resize((100,50))    # but this can ruin the aspect ratio hence we use thumbnail method
-resized_img.save("./Pokedex/resized.png", "png")
+resized_img = img.resize((300,300)) # but this can ruin the aspect ratio hence we use thumbnail method
+resized_img.save(r"c:\Users\cnx36\Documents\7.Projects\1.ZeroToMastery\CompletePythonDeveloper\013_scripting\001_pokedex\Pokedex\resized.png", "png")
 
 box = (100,100,400,400)
 cropped_img = img.crop(box)
-cropped_img.save("./Pokedex/cropped_img.png", "png")
+cropped_img.save(r"c:\Users\cnx36\Documents\7.Projects\1.ZeroToMastery\CompletePythonDeveloper\013_scripting\001_pokedex\Pokedex\cropped.png", "png")
 
-img.thumbnail((100,50))    # it will keep max. 50*50 aspect ratio, it can be like 30*50, but it won't exceed 50 pixels
-img.save("./Pokedex/thumbnailed.png", "png")
+img.thumbnail((100,50)) # it will keep max. 50*50 aspect ratio, it can be 30*50, but it won't exceed 50 pixels
+img.save(r"c:\Users\cnx36\Documents\7.Projects\1.ZeroToMastery\CompletePythonDeveloper\013_scripting\001_pokedex\Pokedex\thumbnail.png", "png")
