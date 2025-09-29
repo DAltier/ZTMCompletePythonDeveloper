37cecf97943e1844813adf14875cb603ce19ff54
DAltier <altierdana@gmail.com>
2025-09-28 22:33:40 -0700

    update routes

--- 015_web_development/002_portfolio/venv/server.py
+++ 015_web_development/002_portfolio/venv/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, make_response
+from flask import Flask, render_template, request, redirect
 import csv
 
 app = Flask(__name__)
@@ -8,40 +8,34 @@ app = Flask(__name__)
 def my_home():
     return render_template('index.html')
 
-@app.route('/works')
-def works_page():
-    return render_template('works.html')
-
-
-@app.route('/favicon.ico')
-def favicon():
-	# Return no content for favicon requests to avoid template lookup errors
-	return make_response('', 204)
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
 def write_to_file(data):
-	with open('database.txt', mode='a') as database:
-		email = data["email"]
-		subject = data["subject"]
-		message = data["message"]
-		file = database.write(f'\n{email},{subject},{message}')
+  with open('database.txt', mode='a') as database:
+    email = data["email"]
+    subject = data["subject"]
+    message = data["message"]
+    file = database.write(f'\n{email},{subject},{message}')
 
 def write_to_csv(data):
-	with open('database.csv', mode='a', newline='') as database2:
-		email = data["email"]
-		subject = data["subject"]
-		message = data["message"]
-		csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		csv_writer.writerow([email,subject,message])
+  with open('database.csv', mode='a', newline='') as database2:
+    email = data["email"]
+    subject = data["subject"]
+    message = data["message"]
+    csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow([email,subject,message])
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-	if request.method == 'POST':
-		try:
-			data = request.form.to_dict()
-			write_to_csv(data)
-			return redirect('/thankyou.html')
-		except:
-			return 'did not save to database'
-	else:
-		return 'something went wrong. Try again!!'
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+      	return 'something went wrong. Try again!!'
