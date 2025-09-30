62170fb990cd1c7215044763d73f5d150beb7f73
DAltier <altierdana@gmail.com>
2025-09-29 22:46:22 -0700

    added database

--- 015_web_development/002_portfolio/venv/server.py
+++ 015_web_development/002_portfolio/venv/server.py
@@ -31,11 +31,14 @@ def write_to_csv(data):
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        try:
-            data = request.form.to_dict()
-            write_to_csv(data)
-            return redirect('/thankyou.html')
-        except:
-            return 'did not save to database'
+      try:
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thankyou.html')
+      except:
+        return 'did not save to database'
     else:
-      	return 'something went wrong. Try again!!'
+      return 'something went wrong. Try again!!'
+
+
+
